4fe24f5b4c9131387c9b2067facfd19adb190631
Sebastian Luna <sebastian.luna.developer@gmail.com>
2025-11-26 19:22:04 -0600

    fix: reject unknown locales in the page as the layout does

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,14 @@
+import { hasLocale } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
+import { notFound } from "next/navigation";
 import LandingPage from "./landing-page";
-import type { Locale } from "@/i18n/routing";
+import { routing } from "@/i18n/routing";
 
 // The whole portfolio is this one page, prerendered in each locale.
 export default async function HomePage({ params }: PageProps<"/[locale]">) {
   const { locale } = await params;
-  setRequestLocale(locale as Locale);
+  if (!hasLocale(routing.locales, locale)) notFound();
+  setRequestLocale(locale);
 
   return <LandingPage />;
 }
